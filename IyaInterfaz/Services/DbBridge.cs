990cf5384b639e8e686b940b3742a4b084e98fd2
Miryunku <MiryunkuDev@outlook.com>
2022-11-20 21:24:40 -0600

    Add project files.

---
+++ IyaInterfaz/Services/DbBridge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Data.Sqlite;
+
+namespace IyaInterfaz.Services
+{
+    public sealed class DbBridge : IDisposable
+    {
+        private bool _disposed = false;
+        private SqliteConnection _connection;
+
+        public DbBridge()
+        {
+            _connection = new SqliteConnection("Data Source=Iya.db;Mode=ReadWrite");
+            _connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) { return; }
+            ((IDisposable)_connection).Dispose();
+            _disposed = true;
+        }
+    }
+}
